f38841801c107d30618f5e441632c3a695c09ca3
ABazh <alexbalex1@yandex.ru>
2023-07-12 16:56:06 +0400

    BREAKING CHANGE: try h2

--- src/main/java/ru/bazhenov/librarianapp/LibrarianAppApplication.java
+++ src/main/java/ru/bazhenov/librarianapp/LibrarianAppApplication.java
@@ -1,10 +1,14 @@
 package ru.bazhenov.librarianapp;
 
+import org.h2.tools.Server;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
 
 import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
@@ -25,5 +29,11 @@ public class LibrarianAppApplication {
 				.setFieldAccessLevel(PRIVATE);
 		return mapper;
 	}
+
+//	@Profile("dev")
+//	@Bean(initMethod = "start", destroyMethod = "stop")
+//	public Server h2Server() throws SQLException {
+//		return Server.createTcpServer("-tcp","-tcpAllowOthers","-tcpPort","9092");
+//	}
 
 }
--- src/main/java/ru/bazhenov/librarianapp/mapper/PersonBookToBookDtoMapper.java
+++ src/main/java/ru/bazhenov/librarianapp/mapper/PersonBookToBookDtoMapper.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 public class PersonBookToBookDtoMapper extends AbstractMapper<PersonBook, BookDto>{
 
     private final ModelMapper modelMapper;
-    @Value(value = "${bookDaysToExpired}")
+    @Value(value = "${book.bookDaysToExpired}")
     private long bookDaysToExpired;
     @Autowired
     public PersonBookToBookDtoMapper(ModelMapper modelMapper) {
