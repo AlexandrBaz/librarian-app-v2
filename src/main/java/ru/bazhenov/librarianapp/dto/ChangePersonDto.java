a2d739f20d4186c4a83ce2200d652a53a2c1b814
ABazh <alexbalex1@yandex.ru>
2023-07-19 01:04:57 +0400

    feat: small bugfixes

--- src/main/java/ru/bazhenov/librarianapp/dto/ChangePersonDto.java
+++ src/main/java/ru/bazhenov/librarianapp/dto/ChangePersonDto.java
@@ -19,23 +19,22 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChangePersonDto extends AbstractDto{
+public class ChangePersonDto extends AbstractDto {
 
     @NotEmpty(message = "Введите Фамилию Имя Отчество")
     @Pattern(regexp = "[А-Я][а-я]+ [А-Я][а-я]+ [А-Я][а-я]+", message = "Формат ввода, с большой буквы, разделенный пробелами")
     private String fullName;
     private String login;
-    //    @NotEmpty(message = "Введите пароль")
-//    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Формат ввода - латиница, не менее 8 символов\n" +
-//            "\n" +
-//            "Содержит хотя бы одну цифру\n" +
-//            "\n" +
-//            "Содержит по крайней мере один нижний альфа-символ и один верхний альфа-символ\n" +
-//            "\n" +
-//            "Содержит по крайней мере один символ в наборе специальных символов (@#%$^ и т.д.)\n" +
-//            "\n" +
-//            "Не содержит пробелов, табуляции и т.д.")
     private String password;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Формат ввода - латиница, не менее 8 символов\n" +
+            "\n" +
+            "Содержит хотя бы одну цифру\n" +
+            "\n" +
+            "Содержит по крайней мере один нижний альфа-символ и один верхний альфа-символ\n" +
+            "\n" +
+            "Содержит по крайней мере один символ в наборе специальных символов (@#%$^ и т.д.)\n" +
+            "\n" +
+            "Не содержит пробелов, табуляции и т.д.")
     private String newPassword;
     @NotEmpty(message = "Введите Ваш email")
     @Email
